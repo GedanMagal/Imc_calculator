e1a6b2cf6bce47dfd1fcca1b6a4c7cd5af618f0d
gedanmagal <gedanycar@gmail.com>
2020-06-11 21:29:46 -0300

    fix bugs

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class _HomeState extends State<Home> {
     heightController.text = "";
     setState(() {
       _result = "Informe seus dados";
+      _formKey = GlobalKey<FormState>();
     });
   }
 
